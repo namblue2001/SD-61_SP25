847151fd5bd8aa4c66eb363b76ad908ea2d5f400
nampnph29389 <128914632+nampnph29389@users.noreply.github.com>
2025-02-15 09:22:01 +0700

    Add files via upload

--- StyleTee/StyleTee/Models/DiaChi.cs
+++ StyleTee/StyleTee/Models/DiaChi.cs
@@ -1,11 +1,32 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace StyleTee.Models
 {
 	public class DiaChi
 	{
-		public DiaChi()
-		{
-		}
+		[Key]
+		public Guid ID_DiaChi { get; set; }
+
+        [Required(ErrorMessage = "ID tài khoản là bắt buộc.")]
+        public Guid ID_TaiKhoan { get; set; }
+
+        [Required(ErrorMessage = "Số nhà là bắt buộc.")]
+        public string soNha { get; set; }
+
+        [Required(ErrorMessage = "Xã là bắt buộc.")]
+        public string xa { get; set; }
+
+        [Required(ErrorMessage = "Huyện là bắt buộc.")]
+        public string huyen { get; set; }
+
+        [Required(ErrorMessage = "Tỉnh/Thành phố là bắt buộc.")]
+        public string tinhThanhPho { get; set; }
+
+        [RegularExpression("^(Hoạt động|Ngừng hoạt động)$", ErrorMessage = "Trạng thái chỉ có thể là 'Hoạt động' hoặc 'Ngừng hoạt động'.")]
+        public string trangThai { get; set; }
+
+		public TaiKhoan TaiKhoan { get; set; }
 	}
 }
 
--- StyleTee/StyleTee/Models/TaiKhoan.cs
+++ StyleTee/StyleTee/Models/TaiKhoan.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace StyleTee.Models
 {
 	public class TaiKhoan
 	{
-		public Gui	}
+		[Key]
+        [Required(ErrorMessage = "ID tài khoản là bắt buộc.")]
+        public Guid ID_TaiKhoan { get; set; }
+
+        [Required(ErrorMessage = "Tài khoản là bắt buộc.")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Tài khoản phải có độ dài từ 5 đến 20 ký tự.")]
+        public string taiKhoan { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu là bắt buộc.")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Mật khẩu phải có độ dài tối thiểu 8 ký tự và tối đa 20 ký tự.")]
+        [DataType(DataType.Password)]
+        public string matKhau { get; set; }
+
+        [EmailAddress(ErrorMessage = "E-mail không hợp lệ.")]
+        [Required(ErrorMessage = "E-mail là bắt buộc.")]
+        public string email { get; set; }
+
+		public string hoTen { get; set; }
+
+        [Phone(ErrorMessage = "Số điện thoại không hợp lệ.")]
+        public string soDienThoai { get; set; }
+
+		public string anhDaiDien { get; set; }
+
+        [RegularExpression("^(Nam|Nu)$", ErrorMessage = "Giới tính chỉ có thể là 'Nam' hoặc 'Nữ'.")]
+        public string gioiTinh { get; set; }
+
+		public DateTime? ngaySinh { get; set; }
+
+        [RegularExpression("^(Hoạt động|Ngừng hoạt động)$", ErrorMessage = "Trạng thái chỉ có thể là 'Hoạt động' hoặc 'Ngừng hoạt động'.")]
+        public string trangThai { get; set; }
+
+        public Guid ID_VaiTro { get; set; }
+
+        public VaiTro VaiTro { get; set; }
+    }
 }
 
