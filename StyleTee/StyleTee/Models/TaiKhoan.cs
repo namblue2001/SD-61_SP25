977b724d8830522557f6d948d84a379155b91500
nampnph29389 <128914632+nampnph29389@users.noreply.github.com>
2025-02-21 21:29:41 +0700

    Add files via upload

--- StyleTee/StyleTee/Models/TaiKhoan.cs
+++ StyleTee/StyleTee/Models/TaiKhoan.cs
@@ -6,7 +6,7 @@ namespace StyleTee.Models
 	public class TaiKhoan
 	{
 		[Key]
-        
+        [Required(ErrorMessage = "ID tài khoản là bắt buộc.")]
         public Guid ID_TaiKhoan { get; set; }
 
         [Required(ErrorMessage = "Tài khoản là bắt buộc.")]
@@ -19,25 +19,32 @@ namespace StyleTee.Models
         public string matKhau { get; set; }
 
         [EmailAddress(ErrorMessage = "E-mail không hợp lệ.")]
-        //[Required(ErrorMessage = "E-mail là bắt buộc.")]
-        public string? email { get; set; }
+        [Required(ErrorMessage = "E-mail là bắt buộc.")]
+        public string email { get; set; }
 
-		public string ?hoTen { get; set; }
+        [Required(ErrorMessage = "Họ tên là bắt buộc.")]
+        [StringLength(50, MinimumLength = 7, ErrorMessage = "Họ tên phải có độ dài tối thiểu 7 ký tự và tối đa 50 ký tự.")]
+        public string hoTen { get; set; }
 
+        [Required(ErrorMessage = "Số điện thoại là bắt buộc.")]
         [Phone(ErrorMessage = "Số điện thoại không hợp lệ.")]
-        public string ?soDienThoai { get; set; }
+        public string soDienThoai { get; set; }
 
-        //[RegularExpression("^(Nam|Nu)$", ErrorMessage = "Giới tính chỉ có thể là 'Nam' hoặc 'Nữ'.")]
-        public string ?gioiTinh { get; set; }
+		public string? anhDaiDien { get; set; }
+
+        [RegularExpression("^(Nam|Nu)$", ErrorMessage = "Giới tính chỉ có thể là 'Nam' hoặc 'Nữ'.")]
+        public string? gioiTinh { get; set; }
 
 		public DateTime? ngaySinh { get; set; }
 
-        //[RegularExpression("^(Hoạt động|Ngừng hoạt động)$", ErrorMessage = "Trạng thái chỉ có thể là 'Hoạt động' hoặc 'Ngừng hoạt động'.")]
-        public string ?trangThai { get; set; }
+        [Required(ErrorMessage = "Trạn thái là bắt buộc.")]
+        [RegularExpression("^(Hoạt động|Ngừng hoạt động)$", ErrorMessage = "Trạng thái chỉ có thể là 'Hoạt động' hoặc 'Ngừng hoạt động'.")]
+        public string trangThai { get; set; }
 
-        public Guid ID_VaiTro { get; set; }
+        [Required(ErrorMessage = "Tên vai trò là bắt buộc.")]
+        [RegularExpression("^(Quản lý|Nhân viên|Khách hàng)$", ErrorMessage = "Vai trò chỉ có thể là Quản lý , Nhân viên hay Khách hàng .")]
+        public string tenVaiTro { get; set; }
 
-        public VaiTro VaiTro { get; set; }
     }
 }
 
