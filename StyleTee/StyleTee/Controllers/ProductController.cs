75a9b54285e35fdd29dcaeef379a3273633d2fa3
Hiệu Văn <vanhieu2506@outlook.com>
2025-02-24 21:23:24 +0700

    aa

--- StyleTee/StyleTee/Controllers/ProductController.cs
+++ StyleTee/StyleTee/Controllers/ProductController.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StyleTee.Models;
 using StyleTee.Data;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Globalization;
 
 namespace StyleTee.Controllers
 {
@@ -15,10 +16,17 @@ namespace StyleTee.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            var sanPhams = await _context.SanPham.ToListAsync();
-            return View(sanPhams);
+            var sanPhams = from s in _context.SanPham
+                           select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                sanPhams = sanPhams.Where(s => s.tenSanPham.Contains(searchString));
+            }
+
+            return View(await sanPhams.ToListAsync());
         }
 
         // GET: Product/Create
@@ -31,27 +39,73 @@ namespace StyleTee.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(SanPham sanPham)
         {
-            
+            try
+            {
+                var product = new SanPham
+                {
+                    ID_SanPham = Guid.NewGuid(),
+                    tenSanPham = sanPham.tenSanPham,
+                    moTa = sanPham.moTa,
+                    giaGoc = sanPham.giaGoc,
+                    trangThai = sanPham.trangThai
+                };
+                _context.SanPham.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // GET: Product/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var sanPham = await _context.SanPham.FindAsync(id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+            return View(sanPham);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, SanPham sanPham)
+        {
+            if (id != sanPham.ID_SanPham)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
                 try
                 {
-                    
-                    var product = new SanPham
-                    {
-                        ID_SanPham = Guid.NewGuid(),
-                        tenSanPham = sanPham.tenSanPham,
-                        moTa = sanPham.moTa,
-                        giaGoc = sanPham.giaGoc,
-                        trangThai = sanPham.trangThai
-                    };
-                    _context.SanPham.Add(product);
+                    _context.Update(sanPham);
                     await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Index)); // Chuyển hướng đến trang danh sách sản phẩm
                 }
-                catch (Exception)
+                catch (DbUpdateConcurrencyException)
                 {
-                    // Xử lý lỗi nếu cần
-                    throw;
+                    if (!SanPhamExists(sanPham.ID_SanPham))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
                 }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sanPham);
+        }
+
+        private bool SanPhamExists(Guid id)
+        {
+            return _context.SanPham.Any(e => e.ID_SanPham == id);
         }
     }
 }
