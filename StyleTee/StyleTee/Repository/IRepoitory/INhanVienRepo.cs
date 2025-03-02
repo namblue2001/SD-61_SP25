7ba67df9bd859add5275def61f1efa3620bb043f
Linh Chelsea <96240827+LinhChel@users.noreply.github.com>
2025-03-02 19:26:35 +0700

    Add files via upload

---
+++ StyleTee/StyleTee/Repository/IRepoitory/INhanVienRepo.cs
@@ -0,0 +1,13 @@
+﻿using StyleTee.Models;
+
+namespace StyleTee.Repository.IRepoitory
+{
+    public interface INhanVienRepo
+    {
+        Task<IEnumerable<TaiKhoan>> GetAllAsync();
+        Task<TaiKhoan> GetByIdAsync(Guid id);
+        Task AddAsync(TaiKhoan nhanvien);
+        Task UpdateAsync(TaiKhoan nhanvien);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ StyleTee/StyleTee/Repository/Repository/NhanVienRepo.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+using StyleTee.Data;
+using StyleTee.Models;
+using StyleTee.Repository.IRepoitory;
+
+namespace StyleTee.Repository.Repository
+{
+    public class NhanVienRepo : INhanVienRepo
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public NhanVienRepo(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task AddAsync(TaiKhoan nhanvien)
+        {
+            _dbcontext.TaiKhoan.Add(nhanvien);
+            await _dbcontext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var staff = await _dbcontext.TaiKhoan.FirstOrDefaultAsync(x => x.ID_TaiKhoan == id);
+            if (staff != null)
+            {
+                _dbcontext.TaiKhoan.Remove(staff);
+                await _dbcontext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<TaiKhoan>> GetAllAsync()
+        {
+            return await _dbcontext.TaiKhoan.ToListAsync();
+        }
+
+        public async Task<TaiKhoan> GetByIdAsync(Guid id)
+        {
+
+            return await _dbcontext.TaiKhoan.FirstOrDefaultAsync(x => x.ID_TaiKhoan == id);
+        }
+
+        public async Task UpdateAsync(TaiKhoan nhanvien)
+        {
+            _dbcontext.TaiKhoan.Update(nhanvien);
+            await _dbcontext.SaveChangesAsync();
+        }
+    }
+}
