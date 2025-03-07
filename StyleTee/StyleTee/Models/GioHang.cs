9ec00b7ea9bb5f9c22ede71f23ca3954cdca035c
Linh Chelsea <96240827+LinhChel@users.noreply.github.com>
2025-03-07 20:14:01 +0700

    Add files via upload

---
+++ StyleTee/StyleTee/Models/GioHang.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StyleTee.Models
+{
+    public class GioHang
+    {
+        [Key]
+        public Guid ID_GioHang { get; set; }
+        public Guid ID_TaiKhoan { get; set; }
+        public DateTime ngayTao { get; set; }
+        public TaiKhoan TaiKhoan { get; set; }
+        public ICollection<GioHangChiTiet> GioHangChiTiet { get; set; }
+
+    }
+}
---
+++ StyleTee/StyleTee/Models/GioHangChiTiet.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace StyleTee.Models
+{
+    public class GioHangChiTiet
+    {
+        [Key]
+        public Guid ID_GioHangChiTiet { get; set; }
+        public Guid ID_GioHang { get; set; }
+        public Guid ID_SanPhamChiTiet { get; set; }
+        [Required(ErrorMessage = "Bạn chưa nhập giá")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal donGia { get; set; }
+        public int soLuong { get; set; }
+        // hiển thị màu sắc, size, xuất xứ, chất liệu, kiểu dáng
+        public virtual SanPhamChiTiet SanPhamChiTiet { get; set; }
+        public virtual GioHang GioHang { get; set; }
+    }
+}
