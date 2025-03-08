f76651307a29a430819bdc778d2ece0ba4e0a91e
Linh Chelsea <96240827+LinhChel@users.noreply.github.com>
2025-03-08 09:23:05 +0700

    Update SanPham.cs

--- StyleTee/StyleTee/Models/SanPham.cs
+++ StyleTee/StyleTee/Models/SanPham.cs
@@ -16,6 +16,7 @@ namespace StyleTee.Models
         public bool trangThai { get; set; }
         public DanhMuc DanhMuc { get; set; }
         public DateTime ngayTao {get;set;}
+        public string anhDaiDien {get;set;}
         public ICollection<SanPhamChiTiet> SanPhamChiTiet { get; set; }
     }
 }
