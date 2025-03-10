036aabca2b0b69d64967977cbec3a0de098a8324
nampnph29389 <128914632+nampnph29389@users.noreply.github.com>
2025-03-10 08:39:13 +0700

    Update SanPhamController.cs

--- StyleTee/StyleTee/Areas/QuanLy/Controllers/SanPhamController.cs
+++ StyleTee/StyleTee/Areas/QuanLy/Controllers/SanPhamController.cs
@@ -1,1 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StyleTee.Data;
+using StyleTee.Models;
+using StyleTee.Models.SanPhamVaThuocTinh;
 
+namespace StyleTee.Areas.QuanLy.Controllers.SanPhamVaThuocTinh
+{
+    [Area("QuanLy")]
+    [Route("QuanLy")]
+    [Route("QuanLy/SanPham")]
+    public class SanPhamController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SanPhamController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            Trang();
+            var applicationDbContext = _context.SanPham;
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        [Route("Index")]
+        [HttpPost]
+        public async Task<IActionResult> Index(string ten)
+        {
+            Trang();
+            return _context.SanPham.Where(n => n.TenSanPham.ToLower().Contains(ten.ToLower())).OrderBy(a => a.TrangThai == "Hoạt động" ? 0 : 1).ThenBy(a => a.TenSanPham) != null ?
+                          View(await _context.SanPham.Where(n => n.TenSanPham.ToLower().Contains(ten.ToLower())).OrderBy(a => a.TrangThai == "Hoạt động" ? 0 : 1).ThenBy(a => a.TenSanPham).ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.SanPham'  is null.");
+        }
+
+        [Route("Create")]
+        public IActionResult Create()
+        {
+            Trang();
+            ViewData["ID_DanhMuc"] = new SelectList(_context.DanhMuc, "ID_DanhMuc", "TenDanhMuc");
+            SanPham sanpham = new SanPham();
+            return View(sanpham);
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SanPham sanPham, IFormFile formFile)
+        {
+            Trang();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "LayoutAdmin/img/SanPham", formFile.FileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                formFile.CopyTo(stream);
+            }
+            sanPham.TrangThai = "Hoạt động";
+            sanPham.AnhDaiDien = formFile.FileName;
+            try
+            {
+                sanPham.ID_SanPham = Guid.NewGuid();
+                _context.Add(sanPham);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                ViewData["ID_DanhMuc"] = new SelectList(_context.DanhMuc, "ID_DanhMuc", "TenDanhMuc", sanPham.ID_DanhMuc);
+                return View(sanPham);
+            }
+        }
+
+        [Route("Edit")]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            Trang();
+            var sanPham = _context.SanPham.FirstOrDefault(a => a.ID_SanPham == id);
+            ViewData["ID_DanhMuc"] = new SelectList(_context.DanhMuc, "ID_DanhMuc", "TenDanhMuc", sanPham.ID_DanhMuc);
+            return View(sanPham);
+        }
+
+        [Route("Edit")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(SanPham sanPham, IFormFile? formFile)
+        {
+            Trang();
+            if (formFile != null)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "LayoutAdmin/img/SanPham", formFile.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    formFile.CopyTo(stream);
+                }
+                sanPham.AnhDaiDien = formFile.FileName;
+            }
+            try
+            {
+                _context.Update(sanPham);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception)
+            {
+                ViewData["ID_DanhMuc"] = new SelectList(_context.DanhMuc, "ID_DanhMuc", "TenDanhMuc", sanPham.ID_DanhMuc);
+                return View(sanPham);
+            }
+
+        }
+
+
+    }
+
+}
