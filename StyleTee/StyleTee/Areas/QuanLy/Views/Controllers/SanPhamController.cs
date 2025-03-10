98ec048df7eff2c883567268f37412a19b31323f
nampnph29389 <128914632+nampnph29389@users.noreply.github.com>
2025-03-10 08:31:27 +0700

    Update SanPhamController.cs

--- StyleTee/StyleTee/Areas/QuanLy/Views/Controllers/SanPhamController.cs
+++ StyleTee/StyleTee/Areas/QuanLy/Views/Controllers/SanPhamController.cs
@@ -1,1 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StyleTee.Data;
+using StyleTee.Models;
+using StyleTee.Models.SanPhamVaThuocTinh;
 
+namespace StyleTee.Areas.QuanLy.Controllers.SanPhamVaThuocTinh
+{
+    [Area("QuanLy")]
+    [Route("QuanLy")]
+    [Route("QuanLy/SanPham")]
+    public class SanPhamController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SanPhamController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        
+        public void Trang()
+        {
+            TempData["Trang"] = "Quản lý sản phẩm";
+        }
+
+        private bool SanPhamExists(Guid id)
+        {
+            return (_context.SanPham?.Any(e => e.ID_SanPham == id)).GetValueOrDefault();
+        }
+    }
+}
