e57faa0295383aaed1c97dfd3cfa1e3a3a684716
nampnph29389 <128914632+nampnph29389@users.noreply.github.com>
2025-03-14 21:38:47 +0700

    Update HomeController.cs

--- StyleTee/StyleTee/Controllers/HomeController.cs
+++ StyleTee/StyleTee/Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace StyleTee.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger , ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult HoSoQuanLy()
