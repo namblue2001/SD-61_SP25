744ca635f1f246646134628240ed61a902c0f25b
nampnph29389 <128914632+nampnph29389@users.noreply.github.com>
2025-03-16 15:24:29 +0700

    Update ApplicationDbContext.cs

--- StyleTee/StyleTee/Data/ApplicationDbContext.cs
+++ StyleTee/StyleTee/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using StyleTee.Models;
+using StyleTee.Models.PhieuGiamGiaVaKhuyenMai;
 
 namespace StyleTee.Data;
 
