f09b3b2f1d6944f8e0d691d3ec24fb90fcb0cc65
nampnph29389 <128914632+nampnph29389@users.noreply.github.com>
2025-03-20 14:42:50 +0700

    Update KhachHangController.cs

--- StyleTee/StyleTee/Areas/NhanVien/Controllers/KhachHangController.cs
+++ StyleTee/StyleTee/Areas/NhanVien/Controllers/KhachHangController.cs
@@ -1,1 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StyleTee.Data;
+using StyleTee.Models;
+
+namespace StyleTee.Areas.NhanVien.Controllers
+{
+    [Area("NhanVien")]
+    [Route("NhanVien")]
+    [Route("NhanVien/KhachHang")]
+    public class KhachHangController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KhachHangController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            Trang();
+            return _context.TaiKhoan.Where(a => a.tenVaiTro == "Khách hàng") != null ?
+                        View(await _context.TaiKhoan.Where(a => a.tenVaiTro == "Khách hàng").ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.TaiKhoanDangNhap'  is null.");
+        }
+
+        [Route("Index")]
+        [HttpPost]
+        public async Task<IActionResult> Index(string? ten)
+        {
+            Trang();
+            if (ten != null)
+            {
+                return _context.TaiKhoan.Where(n => n.hoTen.ToLower().Contains(ten.ToLower()) && n.tenVaiTro == "Khách hàng").OrderBy(a => a.trangThai == "Hoạt động" ? 0 : 1).ThenBy(a => a.hoTen) != null ?
+                              View(await _context.TaiKhoanDangNhap.Where(n => n.hoTen.ToLower().Contains(ten.ToLower()) && n.tenVaiTro == "Khách hàng").OrderBy(a => a.trangThai == "Hoạt động" ? 0 : 1).ThenBy(a => a.hoTen).ToListAsync()) :
+                              Problem("Entity set 'ApplicationDbContext.NhaCungCap'  is null.");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [Route("Details")]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            Trang();
+            var taiKhoan = await _context.TaiKhoan.Include(a => a.DiaChi).FirstOrDefaultAsync(m => m.ID_TaiKhoan == id);
+
+            return View(taiKhoan);
+        }
+
+        public void Trang()
+        {
+            TempData["Trang"] = "Theo dõi khách hàng";
+        }
+
+    }
+}
 
